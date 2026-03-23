1565bb2cf28b481b320f43b679eb59d237afd10c
mrtokyo33 <mrtokyo33sec@gmail.com>
2026-03-23 14:28:04 -0300

    feat: render texture and implement basic player movement

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,29 @@ constexpr int WINDOW_HEIGHT     = 720;
 constexpr int FPS_TARGET        = 60;
 constexpr int FRAME_DURATION_MS = 1000 / FPS_TARGET;
 
- int main(int argc, char* argv[]) {
+SDL_Texture* loadTexture(SDL_Renderer* renderer, const char* path) {
+    // fluxo: arquivo PNG -> SDL_Surface (memória RAM) -> SDL_Texture (GPU) -> desenhar na tela
+
+    SDL_Surface* surface = IMG_Load(path); // carrega a imagem na RAM (CPU Memory)
+
+    if (!surface) {
+        std::cerr << "Error Loading Image from: " << path << ":" << IMG_GetError() << std::endl;
+        return nullptr;
+    }
+
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface); // converte a imagem da RAM em GPU para rendereização
+
+    SDL_FreeSurface(surface);   // libera a memória RAM
+
+    if (!texture) {
+        std::cerr << "Error Creating Texture from: " << path << ":" << SDL_GetError() << std::endl;
+        return nullptr;
+    }
+
+    return texture;
+}
+
+int main(int argc, char* argv[]) {
     if (SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO) != 0) {   // se der erro ao inicializar o audio e video
         std::cout << "Error Initializing SDL2: " << SDL_GetError() << std::endl;
         return 1;
@@ -70,6 +92,26 @@ constexpr int FRAME_DURATION_MS = 1000 / FPS_TARGET;
         return 1;
     }
 
+    SDL_Texture* textureTest = loadTexture(renderer, "./assets/image/playerDown.png");
+
+    if (!textureTest) {
+        std::cerr << "WARNING! Test Image not founded!" << std::endl;
+        std::cerr << "The Game will run without sprite for a while." << std::endl;
+    }
+
+    int spriteWidth  = 0;
+    int spriteHeight = 0;
+    
+    if(textureTest) {
+        SDL_QueryTexture(textureTest, NULL, NULL, &spriteWidth, &spriteHeight); // obtém a largura e altura do sprite em px
+        std::cout << "Sprite Loaded: " << spriteWidth << "x" << spriteHeight << "px" << std::endl;
+    }
+
+    float spriteX = (WINDOW_WIDTH - spriteWidth) / 2.0f;
+    float spriteY = (WINDOW_HEIGHT - spriteHeight) / 2.0f;
+
+    float spriteSpd = 200.0f;
+
     bool running = true; 
 
     // O SDL_Event é uma struct que guarda os eventos: tudo que acontece durante a execução do programa
@@ -95,11 +137,39 @@ constexpr int FRAME_DURATION_MS = 1000 / FPS_TARGET;
             }
         }
 
-        SDL_SetRenderDrawColor(renderer, 12, 12, 12, 255);
+        // array que guarda o state de todas as teclas pressionadas
+        const Uint8* keyboardState = SDL_GetKeyboardState(NULL);
 
+        // lógica de movimentação usando deltaTime
+        if (keyboardState[SDL_SCANCODE_UP]) {
+            spriteY -= spriteSpd * deltaTime;
+        }
+        if (keyboardState[SDL_SCANCODE_DOWN]) {
+            spriteY += spriteSpd * deltaTime;
+        }
+        if (keyboardState[SDL_SCANCODE_LEFT]) {
+            spriteX -= spriteSpd * deltaTime;
+        }
+        if (keyboardState[SDL_SCANCODE_RIGHT]) {
+            spriteX += spriteSpd * deltaTime;
+        }
+
+        SDL_SetRenderDrawColor(renderer, 12, 12, 12, 255);
         SDL_RenderClear(renderer);
+
+        if (textureTest) {
+            SDL_Rect dest;  // literalmente um retangulo, com susas posições (x,y) e tamanho (w,h)
+            dest.x = static_cast<int>(spriteX);
+            dest.y = static_cast<int>(spriteY);
+            dest.w = spriteWidth;
+            dest.h = spriteHeight;
+
+            SDL_RenderCopy(renderer, textureTest, NULL, &dest);
+        }
+
         SDL_RenderPresent(renderer);
 
+        // lógica para deixar fluído de acordo com o FRAME_DURATION_MS, para um frame nao ser muito curto e sobrecarregar a CPU
         Uint32 frame_time = SDL_GetTicks() - current_tick;
 
         if (frame_time < FRAME_DURATION_MS) {
@@ -108,6 +178,11 @@ constexpr int FRAME_DURATION_MS = 1000 / FPS_TARGET;
     }
 
     // limpeza já que o jogo não está mais rodando
+
+    if (textureTest) {
+        SDL_DestroyTexture(textureTest);
+    }
+
     SDL_DestroyRenderer(renderer);  
     SDL_DestroyWindow(window);
 
